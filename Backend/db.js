863660700b8217a8881e1cea8887f7c5dd923a77
farhan-awan67 <farhanawxn@gmail.com>
2024-11-21 12:30:52 +0500

    small change

--- Backend/db.js
+++ Backend/db.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
   try {
-    console.log(typeof process.env.MONGO_URL);
     await mongoose.connect(process.env.MONGO_URL);
     console.log("DB connected");
   } catch (error) {
